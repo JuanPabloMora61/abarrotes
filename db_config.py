b64a288b33cf6d88d3dab2926ad1c479095ee9cd
jpmc <jmoracafe@gmail.com>
2025-08-13 20:00:04 -0600

    se parsea el port a int

--- db_config.py
+++ db_config.py
@@ -8,6 +8,8 @@ app.config['MYSQL_DATABASE_USER'] = os.getenv("MYSQL_DATABASE_USER")
 app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("MYSQL_DATABASE_PASSWORD")
 app.config['MYSQL_DATABASE_DB'] = os.getenv("MYSQL_DATABASE_DB")
 app.config['MYSQL_DATABASE_HOST'] = os.getenv("MYSQL_DATABASE_HOST")
-app.config['MYSQL_DATABASE_PORT'] = os.getenv("MYSQL_DATABASE_PORT")
+port = os.getenv("MYSQL_DATABASE_PORT")
+if port:
+    app.config['MYSQL_DATABASE_PORT'] = int(port)
 
 mysql.init_app(app)
