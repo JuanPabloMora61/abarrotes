2eb09b6f7e51877ac813c454eedd8c25aff7be3b
jpmc <jmoracafe@gmail.com>
2025-08-13 19:25:40 -0600

    Se agregan variables de entorno

--- db_config.py
+++ db_config.py
@@ -1,10 +1,12 @@
+import os
 from app import app
 from flaskext.mysql import MySQL
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
-app.config['MYSQL_DATABASE_DB'] = 'abarrotes'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = os.getenv("MYSQL_DATABASE_USER")
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("MYSQL_DATABASE_PASSWORD")
+app.config['MYSQL_DATABASE_DB'] = os.getenv("MYSQL_DATABASE_DB")
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("MYSQL_DATABASE_HOST")
+
 mysql.init_app(app)
